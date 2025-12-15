5f3fa80fd92b5012a163dfdaecc05a70e24dc322
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2025-12-15 18:59:24 +0400

    Removed router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,46 @@
-<script setup></script>
+<script setup>
+import {
+    ref
+} from 'vue'
+import { useRouter } from 'vue-router';
+
+import ParticipantForm from './ParticipantForm.vue';
+import PassPhone from './PassPhone.vue';
+
+const participants = ref([]);
+
+const STATE = {
+    inputs: "inputs",
+    reveals: "reveals"
+};
+
+const gameState = ref(STATE.inputs);
+
+const addParticipant = (name, text) => {
+    if (name === '') return;
+    participants.value.push({
+        name,
+        text
+    });
+};
+
+const startGame = () => {
+    if (participants.value.length < 3) {
+        alert('At least 3 participants are required to start the game.');
+        return;
+    }
+    gameState.value = STATE.reveals;
+};
+
+const matchingDone = () => {
+}
+</script>
 
 <template>
-<router-view />
+<transition name="slide" mode="out-in">
+    <ParticipantForm v-if="gameState == STATE.inputs" :key="STATE.inputs" :participants="participants" @addParticipant="addParticipant" @startGame="startGame" />
+    <PassPhone v-else-if="gameState == STATE.reveals" :key="STATE.reveals" @done="matchingDone" :participantList="participants" />
+</transition>
 </template>
 
 <style>
